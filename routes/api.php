713f020ec61f05380937498a4265648288d0ce17
unknown <muhammadirfan6777@gmail.com>
2021-06-22 12:38:48 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
-
-
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\BalanceController;
 use App\Http\Controllers\MutationController;
 /*
@@ -34,7 +33,6 @@ Route::get('/product', [ProductController::class, 'index']);
 Route::post('/product/store', [ProductController::class, 'store']);
 Route::post('/product/update', [ProductController::class, 'update']);
 Route::delete('/product/delete/{id}', [ProductController::class, 'delete']);
-
 Route::get('/product/detail/{id}', [ProductController::class, 'detail']);
 
 // API BALANCE
@@ -45,3 +43,9 @@ Route::post('/balance/decrease', [BalanceController::class, 'decrease']);
 // API MUTATION
 Route::get('/mutations/{id}', [MutationController::class, 'index']);
 Route::post('/mutation/store', [MutationController::class, 'store']);
+
+//route Transaction
+Route::get('/transaction/{id}', [TransactionController::class, 'index']);
+Route::post('/transaction/tambah', [TransactionController::class, 'tambah']);
+Route::get('/transaction/detail/{id}', [TransactionController::class, 'detail']);
+Route::post('/transaction/update', [TransactionController::class, 'update']);
