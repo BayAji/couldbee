237878da491991fb6f8d00c4112105792119fe94
Alfianmuh12 <59167735+Alfianmuh12@users.noreply.github.com>
2021-06-22 17:16:14 +0700

    Add balance and mutation

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
-
+use App\Http\Controllers\BalanceController;
+use App\Http\Controllers\MutationController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -30,3 +31,12 @@ Route::post('/product/store', [ProductController::class, 'store']);
 Route::post('/product/update', [ProductController::class, 'update']);
 Route::delete('/product/delete/{id}', [ProductController::class, 'delete']);
 Route::get('/product/detail/{id}', [ProductController::class, 'detail']);
+
+// API BALANCE
+Route::get('/balance/detail/{id}', [BalanceController::class, 'detail']);
+Route::post('/balance/increase', [BalanceController::class, 'increase']);
+Route::post('/balance/decrease', [BalanceController::class, 'decrease']);
+
+// API MUTATION
+Route::get('/mutations/{id}', [MutationController::class, 'index']);
+Route::post('/mutation/store', [MutationController::class, 'store']);
